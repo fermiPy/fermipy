8b9bd5b7d5296c2243bbbd567533db4932784249
Matthew Wood <mdwood137@gmail.com>
2015-03-20 17:09:36 -0700

    Adding simple installation script.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='fermipy',
+      version='0.1.0',
+      author='The Fermipy developers',
+      packages=['fermipy'],
+      url = "https://github.com/fermiPy/fermipy",
+      scripts = [],
+      data_files=[],
+      install_requires=['numpy >= 1.8.0',
+                        'matplotlib >= 1.2.0',
+                        'astropy >= 0.3',
+                        'scipy >= 0.13'])
