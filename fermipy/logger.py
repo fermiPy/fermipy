01eef7cd88b0b474e5c471ae8628aaf7fa9f4e1f
Matthew Dunseth Wood <mdwood137@gmail.com>
2015-04-15 15:27:20 -0700

    Added class that captures stdout.

--- fermipy/logger.py
+++ fermipy/logger.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 import logging
 import logging.config
 import yaml
@@ -27,7 +28,7 @@ def logLevel(level):
 class Logger(object):
     """This class provides helper functions which facilitate creating
     instances of the built-in logger class."""
-            
+    
     @staticmethod
     def setup(config=None,logfile=None):
         """This method sets up the default configuration of the
@@ -35,7 +36,8 @@ class Logger(object):
         Logger instances will inherit this configuration."""
 
         if config is None:
-            configpath = os.path.join(fermipy.PACKAGE_ROOT,'config','logging.yaml')
+            configpath = os.path.join(fermipy.PACKAGE_ROOT,'config',
+                                      'logging.yaml')
             with open(configpath,'r') as f:
                 config = yaml.load(f)
 
@@ -66,13 +68,16 @@ class Logger(object):
         logger = logging.getLogger(name)
         logger.setLevel(logging.DEBUG)
         logfile = logfile.replace('.log','') + '.log'
+
+        datefmt = '%Y-%m-%d %H:%M:%S'
+        format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         
         if not logger.handlers:
             fh = logging.FileHandler(logfile)
             fh.setLevel(logging.DEBUG)
             ch = logging.StreamHandler()
             ch.setLevel(loglevel)
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            formatter = logging.Formatter(format,datefmt)
             fh.setFormatter(formatter)
             ch.setFormatter(formatter)
             logger.addHandler(fh)
@@ -81,3 +86,48 @@ class Logger(object):
             logger.handlers[1].setLevel(loglevel)
             
         return logger
+
+class StreamLogger(object):
+    """File-like object to log stdout/stderr using the `logging` module."""
+
+    def __init__(self, name='stdout', logfile=None, quiet=True):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        self.format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        self.datefmt='%Y-%m-%d %H:%M:%S'
+        self.stdout = sys.stdout
+#        logging.basicConfig(level=logging.DEBUG, 
+#                            format=self.format, filename=file)
+
+        self.formatter = logging.Formatter(self.format,self.datefmt)
+        fhdlr = logging.FileHandler(logfile)
+        fhdlr.setFormatter(self.formatter)
+        self.logger.addHandler(fhdlr)
+
+        if not quiet and name == 'stdout':
+            shdlr = logging.StreamHandler(sys.stdout)
+            shdlr.setFormatter(self.formatter)
+            self.logger.addHandler(shdlr)
+        elif not quiet and name == 'stderr':
+            shdlr = logging.StreamHandler(sys.stderr)
+            shdlr.setFormatter(self.formatter)
+            self.logger.addHandler(shdlr)
+
+        sys.stdout = self
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        self.flush()
+        sys.stdout = self.stdout
+        self.logger.handlers = []
+        
+    def write(self, msg, level=logging.DEBUG):
+        msg = msg.rstrip('\n')
+        if len(msg) > 0:
+            self.logger.log(level, msg)
+
+    def flush(self):
+        for handler in self.logger.handlers:
+            handler.flush()
