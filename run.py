2a67f36c01a5633808f724ff0ec93898db9b926b
Matthew Dunseth Wood <mdwood137@gmail.com>
2015-08-28 11:46:06 -0700

    Cleanup example run script.

--- run.py
+++ run.py
@@ -1,11 +1,5 @@
-from fermipy.gtanalysis import *
-from fermipy.roi_model import *
-from fermipy.config import *
-from fermipy.logger import *
-import yaml
-import pprint
+from fermipy.gtanalysis import GTAnalysis
 import argparse
-
 
 usage = "usage: %(prog)s [config file]"
 description = "Run fermipy analysis chain."
@@ -15,11 +9,12 @@ parser.add_argument('--config', default = 'sample_config.yaml')
 
 args = parser.parse_args()
 
-config = ConfigManager.create(args.config)
-
-gta = GTAnalysis(config)
+gta = GTAnalysis(args.config)
 
 gta.setup()
+
+# Iteratiely optimize all components in the ROI
+gta.optimize()
 
 # Fix sources w/ significance < 10
 gta.free_sources(cuts=('Detection_Significance',0,10),free=False)
@@ -31,6 +26,8 @@ gta.free_sources(distance=3.0)
 gta.free_source('mkn421')
 gta.free_source('galdiff')
 gta.free_source('isodiff')
+
+# Free only the normalization of a specific source
 gta.free_norm('3FGL J1129.0+3705')
 
 gta.fit()
@@ -38,10 +35,9 @@ gta.fit()
 # Compute the SED for a source
 gta.sed('mkn421')
 
-# Write the post-fit XML model
-gta.write_xml('fit1.xml')
-
-# Write all information about the ROI as a yaml file 
+# Write the current state of the ROI model -- this will generate XML
+# model files for each component as well as an output analysis
+# dictionary in numpy and yaml formats
 gta.write_roi('fit1')
 
 
