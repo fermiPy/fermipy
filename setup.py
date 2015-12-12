c4e60c55e2df5214a7d54ed21f074ca7f53c7088
Matthew Wood <mdwood137@gmail.com>
2015-12-11 23:34:27 -0800

    Disabling checking on ST python.

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ import sys
 from fermipy.version import get_git_version
 
 ##Check to make sure we're using the python in the STs.
-if 'GLAST_EXT' in os.environ:
-    print("Looks like you're using the SLAC version of the tools.")
-    print("Not checking for correct python.")
-elif 'FERMI_DIR' in os.environ:
-    print("Looks like you're using the FSSC version of the tools.")
-    fermi_dir = os.getenv('FERMI_DIR')
-    os_file = os.__file__
-    if fermi_dir not in os_file:
-        print("Python executable is not the one in $FERMI_DIR/bin.  Exiting.")
-        sys.exit(1)
-else:
-    print("Looks like the Fermi Science Tools are not setup.  Exiting.")
-    sys.exit(1)
+#if 'GLAST_EXT' in os.environ:
+#    print("Looks like you're using the SLAC version of the tools.")
+#    print("Not checking for correct python.")
+#elif 'FERMI_DIR' in os.environ:
+#    print("Looks like you're using the FSSC version of the tools.")
+#    fermi_dir = os.getenv('FERMI_DIR')
+#    os_file = os.__file__
+#    if fermi_dir not in os_file:
+#        print("Python executable is not the one in $FERMI_DIR/bin.  Exiting.")
+#        sys.exit(1)
+#else:
+#    print("Looks like the Fermi Science Tools are not setup.  Exiting.")
+#    sys.exit(1)
 
 setup(name='fermipy',
       version=get_git_version(),
