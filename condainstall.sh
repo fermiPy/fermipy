e3c62771816fd2887b40060ff0fccc045c6f477a
woodmd <mdwood137@gmail.com>
2015-12-16 00:28:22 +0000

    Testing travis/conda shell scripts.

--- condainstall.sh
+++ condainstall.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/miniconda" ]; then
 fi
 
 export PATH="$HOME/miniconda/bin:$PATH"
-conda create -q -n fermi-env -y python=2.7 numpy scipy matplotlib astropy
+conda create -q -n fermi-env -y python=2.7 numpy scipy matplotlib astropy pytest
 source activate fermi-env
 pip install healpy
-#python setup.py install
+python setup.py install
--- condasetup.sh
+++ condasetup.sh
@@ -1,3 +1,5 @@
 source $FERMI_DIR/fermi-init.sh
+export PATH=$HOME/miniconda/bin:$PATH
+source activate fermi-env
 export PATH=$CONDA_ENV_PATH/bin:$PATH
 export PYTHONPATH=$FERMI_DIR/lib/python:$FERMI_DIR/lib
