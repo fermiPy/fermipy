e56baa7b7e9f8097c7c154bec127a9b1e76c464f
Matthew Wood <mdwood137@gmail.com>
2016-01-26 14:18:56 -0800

    Make source localization more robust.

--- fermipy/gtanalysis.py
+++ fermipy/gtanalysis.py
@@ -1397,7 +1397,7 @@ class GTAnalysis(fermipy.config.Configurable):
 
     def localize(self, name, **kwargs):
         """Perform a fit for the best-fit position of this source.
-        
+
         Parameters
         ----------
 
@@ -1419,10 +1419,10 @@ class GTAnalysis(fermipy.config.Configurable):
             existing source map of this source with one corresponding
             to its new location.
 
-        newname : str        
+        newname : str
             Name that will be assigned to the relocalized source model
             when update=True.  If newname is None then the existing
-            source name will be used.            
+            source name will be used.
 
         Returns
         -------
@@ -1439,8 +1439,7 @@ class GTAnalysis(fermipy.config.Configurable):
         # Extract options from kwargs
         config = copy.deepcopy(self.config['localize'])
         config.update(kwargs)
-        config.setdefault('newname',name)
-#                          name.replace(' ', '').lower() + '_reloc')
+        config.setdefault('newname', name)
 
         nstep = config['nstep']
         dtheta_max = config['dtheta_max']
@@ -1464,7 +1463,8 @@ class GTAnalysis(fermipy.config.Configurable):
         self.zero_source(name)
 
         o = {'config': config,
-             'logLike_base' : logLike0 }
+             'fit_success': True,
+             'logLike_base': logLike0 }
 
         deltax = np.linspace(-dtheta_max, dtheta_max, nstep)[:, np.newaxis]
         deltay = np.linspace(-dtheta_max, dtheta_max, nstep)[np.newaxis, :]
@@ -1477,7 +1477,8 @@ class GTAnalysis(fermipy.config.Configurable):
         lnlscan = dict(deltax=deltax,
                        deltay=deltay,
                        logLike=np.zeros((nstep, nstep)),
-                       dlogLike=np.zeros((nstep, nstep)))
+                       dlogLike=np.zeros((nstep, nstep)),
+                       dlogLike_fit=np.zeros((nstep, nstep)))
 
         for i, t in enumerate(scan_skydir):
             # make a copy
@@ -1489,7 +1490,7 @@ class GTAnalysis(fermipy.config.Configurable):
             #            s.set_spatial_model(spatial_model,w)
 
             self.add_source(model_name, s, free=True,
-                            save_source_maps=False)
+                            init_source=False, save_source_maps=False)
             self.fit(update=False)
 
             logLike1 = -self.like()
@@ -1501,23 +1502,34 @@ class GTAnalysis(fermipy.config.Configurable):
         dlogmax = np.max(lnlscan['dlogLike']) - np.min(lnlscan['dlogLike'])
         sigma = (0.5 * dtheta_max ** 2 / dlogmax) ** 0.5
 
-        p0 = (0.0, 0.0, 0.0, sigma, sigma, 0.0)
+        ix, iy = np.unravel_index(np.argmax(lnlscan['dlogLike']),(nstep,nstep))
+        p0 = (0.0, deltax[ix,iy], deltay[ix,iy], sigma, sigma, 0.0)
 
         try:
             popt, pcov = scipy.optimize.curve_fit(parabola, (
                 lnlscan['deltax'], lnlscan['deltay']),
-                                                  lnlscan['dlogLike'].flat, p0)
+                lnlscan['dlogLike'].flat, p0)
         except Exception:
             popt = p0
+            o['fit_success'] = False
             self.logger.error('Localization failed.', exc_info=True)
 
+        offset = (popt[1]**2 + popt[2]**2)**0.5
+        if o['fit_success'] and offset > dtheta_max:
+            o['fit_success'] = False
+            self.logger.error('Position offset larger than scan region:\n '
+                              'offset = %.3f dtheta_max = %.3f' % (offset,dtheta_max))
+            
+        lnlscan['dlogLike_fit'] = parabola((lnlscan['deltax'], lnlscan['deltay']),
+                                           *popt)
+            
         o['lnlscan'] = lnlscan
-
         o['deltax'] = popt[1]
         o['deltay'] = popt[2]
         o['sigmax'] = popt[3]
         o['sigmay'] = popt[4]
         o['theta'] = popt[5]
+        o['offset'] = offset
 
         new_skydir = utils.offset_to_skydir(skydir, popt[1], popt[2],
                                             coordsys=self.config['binning']['coordsys'])
@@ -1529,7 +1541,7 @@ class GTAnalysis(fermipy.config.Configurable):
 
         saved_state.restore()
 
-        if update:
+        if update and o['fit_success']:
 
 #            if newname == name:
 #                raise Exception('Error setting name for new source model.  '
@@ -1537,7 +1549,7 @@ class GTAnalysis(fermipy.config.Configurable):
 #                                'source name.')
 
             self.logger.info(
-                'Updating position: %.3f %.3f' % (o['ra'], o['dec']))
+                'Updating position to: RA %8.3f DEC %8.3f' % (o['ra'], o['dec']))
             s = self.copy_source(name)
             self.delete_source(name)
             s.set_position(new_skydir)
@@ -2736,8 +2748,8 @@ class GTAnalysis(fermipy.config.Configurable):
                              fileio=self.config['fileio'],
                              logging=self.config['logging'])
 
-        model = kwargs.get('model', self.config['model'])
-        return tsg.make_ts_map(self, prefix, model,**kwargs)
+        model = kwargs.get('model', self.config['tsmap']['model'])
+        return tsg.make_ts_map(self, prefix, copy.deepcopy(model), **kwargs)
 
     def _tsmap_pylike(self, prefix, **kwargs):
         """Evaluate the TS for an additional source component at each point
@@ -3750,12 +3762,12 @@ class GTBinnedAnalysis(fermipy.config.Configurable):
                                  logger=self.logger)
 
     def simulate_source(self, name):
-
+        
         data = self.counts_map().counts
         m = self.model_counts_map(name)
-        data += np.random.poisson(m.counts).astype(float)
-
-        # saveSourceMaps?
+        
+        src_data = np.random.poisson(m.counts).astype(float)
+        data += src_data
 
         utils.update_source_maps(self._srcmap_file, {'PRIMARY': data},
                                  logger=self.logger)
