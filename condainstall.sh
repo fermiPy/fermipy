f7c3ff417eb8eb1fb42aa3d01adf3fcae664bfd2
Matthew Wood <mdwood137@gmail.com>
2016-04-14 15:42:38 -0700

    migrate notebooks to fermipy-extras

--- condainstall.sh
+++ condainstall.sh
@@ -20,6 +20,6 @@ fi
 
 conda create -q -n fermi-env -y python=2.7 numpy scipy matplotlib astropy pytest pyyaml ipython
 source activate fermi-env
-conda install -c openastronomy wcsaxes
+conda install -c openastronomy -y wcsaxes
 pip install healpy
 pip install fermipy
