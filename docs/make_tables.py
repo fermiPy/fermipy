8737c8e5b2c8d2bee1393f31f4fd8693813c1fc3
Matthew Wood <mdwood137@gmail.com>
2016-04-19 00:37:51 -0700

    documentation updates

--- docs/make_tables.py
+++ docs/make_tables.py
@@ -1,14 +1,14 @@
 import fermipy.defaults as defaults
 
 for k, v in defaults.__dict__.items():
-
+    
     if not isinstance(v,dict) or k.startswith('_'):
         continue
     
     f = open("config/%s.csv"%k, "w")
 
     if 'output' in k:
-        for optname, optdict in sorted(v.items()):
+        for optname, optdict in v.items():
             f.write('``%s``\t%s\t%s\n'%(optname,optdict[3],optdict[1]))
     else:
         for optname, optdict in sorted(v.items()):
