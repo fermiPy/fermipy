3d8aab82c0833bc2aedadc2b39f2868d50190697
Matthew Dunseth Wood <mdwood137@gmail.com>
2016-05-10 21:20:59 -0700

    Tweak vectorization scheme for evaluation methods.

--- fermipy/spectrum.py
+++ fermipy/spectrum.py
@@ -36,7 +36,15 @@ class SEDFluxFunctor(SEDFunctor):
         super(SEDFluxFunctor,self).__init__(spectrum, scale, emin, emax)
         
     def __call__(self,params):
-        return self._spectrum.eval_flux(self._emin,self._emax,
+
+        params = list(params)
+        for i, p in enumerate(params):
+            params[i] = np.expand_dims(np.array(params[i],ndmin=1),0)
+            
+        emin = np.expand_dims(self.emin,1)
+        emax = np.expand_dims(self.emax,1)
+        
+        return self._spectrum.eval_flux(emin,emax,
                                         params,self.scale)
 
 class SEDEFluxFunctor(SEDFunctor):
@@ -47,8 +55,15 @@ class SEDEFluxFunctor(SEDFunctor):
         super(SEDEFluxFunctor,self).__init__(spectrum, scale, emin, emax)
         
     def __call__(self,params):
-        return self._spectrum.eval_eflux(self._emin,self._emax,
-                                         params,self.scale)
+
+        params = list(params)
+        for i, p in enumerate(params):
+            params[i] = np.expand_dims(np.array(params[i],ndmin=1),0)
+            
+        emin = np.expand_dims(self.emin,1)
+        emax = np.expand_dims(self.emax,1)
+        
+        return self._spectrum.eval_eflux(emin,emax, params,self.scale)
         
 class SpectralFunction(object):
     """Base class for spectral model classes."""
@@ -92,16 +107,31 @@ class SpectralFunction(object):
         return cls.eval_dfde(x,params,scale)*x
 
     @classmethod
-    def integrate(cls, fn, emin, emax, params, scale=1.0, npt=10):
+    def integrate(cls, fn, emin, emax, params, scale=1.0, npt=20):
         """Fast numerical integration method using mid-point rule."""
+
+        emin = np.expand_dims(emin,-1)
+        emax = np.expand_dims(emax,-1)
+
+        params = copy.deepcopy(params)
         
-        xedges = np.linspace(0.0,1.0,npt+1)        
-        logx_edge = np.log(emin) + xedges[:,np.newaxis]*(np.log(emax)-np.log(emin)) 
-        logx = 0.5*(logx_edge[1:,...]+logx_edge[:-1,...])
-        xw = np.exp(logx_edge[1:,...])-np.exp(logx_edge[:-1,...])
-        x = np.exp(logx)
-        dfde = fn(x,params,scale)
-        return np.sum(dfde*xw,axis=0)
+        for i, p in enumerate(params):
+            params[i] = np.expand_dims(params[i],-1)
+            
+#        params = np.expand_dims(params,-1)
+        xedges = np.linspace(0.0,1.0,npt+1)
+        logx_edge = np.log(emin) + xedges*(np.log(emax)-np.log(emin)) 
+        logx = 0.5*(logx_edge[...,1:]+logx_edge[...,:-1])
+        xw = np.exp(logx_edge[...,1:])-np.exp(logx_edge[...,:-1]) 
+        dfde = fn(np.exp(logx),params,scale)
+        return np.sum(dfde*xw,axis=-1)
+        
+        #logx_edge = np.log(emin) + xedges[:,np.newaxis]*(np.log(emax)-np.log(emin)) 
+        #logx = 0.5*(logx_edge[1:,...]+logx_edge[:-1,...])
+        #xw = np.exp(logx_edge[1:,...])-np.exp(logx_edge[:-1,...])        
+        #x = np.exp(logx)
+        #dfde = fn(x,params,scale)
+        #return np.sum(dfde*xw,axis=0)
     
     @classmethod
     def eval_flux(cls, emin, emax, params, scale=1.0):
@@ -137,30 +167,40 @@ class PowerLaw(SpectralFunction):
         return params[0] * (x / scale) ** params[1]
     
     @classmethod
-    def eval_flux(cls, xmin, xmax, params, scale=1.0):
-        phi0 = params[0]
-        index = params[1]
+    def eval_flux(cls, emin, emax, params, scale=1.0):
+
+        phi0 = np.array(params[0],ndmin=1)
+        index = np.array(params[1],ndmin=1)
         x0 = scale
+
+        index1 = index+1
+        m = np.isclose(index1,0.0)
+
+        iindex0 = np.zeros(index.shape)
+        iindex1 = np.zeros(index.shape)
+        iindex1[~m] = 1./index1[~m]
+        iindex0[m] = 1.        
         
-        if np.allclose(index, -1.0):
-            return phi0 * x0 ** (-index) * (np.log(xmax) - np.log(xmin))
-
-        y0 = x0 * phi0 * (xmin / x0) ** (index + 1) / (index + 1)
-        y1 = x0 * phi0 * (xmax / x0) ** (index + 1) / (index + 1)
-        v = y1 - y0
-
-        return y1 - y0
+        v0 = phi0 * x0 ** (-index) * (np.log(emax) - np.log(emin))*iindex0
+        v1 = phi0 * x0 ** (-index) * (emax ** index1 - emin ** index1)*iindex1
+        
+        return v0+v1
+        
+#        y0 = x0 * phi0 * (emin / x0) ** (index + 1) / (index + 1)
+#        y1 = x0 * phi0 * (emax / x0) ** (index + 1) / (index + 1)
+#        v2 = y1 - y0
+#        return v2
 
     @classmethod
-    def eval_eflux(cls,xmin, xmax, params, scale=1.0):
+    def eval_eflux(cls,emin, emax, params, scale=1.0):
 
         params = copy.deepcopy(params)
         params[1] += 1.0        
-        return cls.eval_flux(xmin,xmax,params,scale)*scale
+        return cls.eval_flux(emin,emax,params,scale)*scale
         
     @staticmethod
-    def eval_norm(scale, index, xmin, xmax, flux):
-        return flux / PowerLaw.eval_flux(xmin,xmax, [1.0, index], scale)
+    def eval_norm(scale, index, emin, emax, flux):
+        return flux / PowerLaw.eval_flux(emin,emax, [1.0, index], scale)
 
 
 class LogParabola(SpectralFunction):
