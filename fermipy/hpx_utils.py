495368d49923d22bc6e01750dbae8b85633fe92a
Christoph Deil <Deil.Christoph@gmail.com>
2016-07-20 15:13:15 +0200

    Add fermipy/tests/test_skymap.py

--- fermipy/hpx_utils.py
+++ fermipy/hpx_utils.py
@@ -580,30 +580,3 @@ class HpxToWcsMapping(object):
         if normalize:
             wcs_data_flat *= self._mult_val
         wcs_data.flat = wcs_data_flat
-
-
-if __name__ == "__main__":
-
-    from fermipy.fits_utils import write_fits_image
-    from fermipy.skymap import HpxMap
-
-    n = np.ones((10, 192), 'd')
-    hpx = HPX(4, False, "GAL")
-    hpx.write_fits(n, "test_hpx.fits", clobber=True)
-
-    ebins = np.logspace(2, 5, 8)
-
-    hpx_2 = HPX(1024, False, "GAL", region="DISK(110.,75.,2.)", ebins=ebins)
-    #hpx_2 = HPX(1024,False,"GAL",region="HPX_PIXEL(RING,16,500)",ebins=ebins)
-    npixels = hpx_2.npix
-    print("Npixels", npixels)
-
-    n2 = np.ndarray((8, npixels), 'd')
-    for i in range(8):
-        n2[i].flat = np.arange(npixels)
-
-    hpx_map = HpxMap(n2, hpx_2)
-    wcs, wcs_data = hpx_map.make_wcs_from_hpx(normalize=True)
-
-    wcs_out = hpx_2.make_wcs(3)
-    write_fits_image(wcs_data, wcs_out, "test_hpx_2_wcs.fits")
