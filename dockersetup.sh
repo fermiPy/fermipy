a0192d4cb0200382d0be9fcaca8ecc0676325b27
Matthew Wood <mdwood137@gmail.com>
2016-08-10 12:09:01 -0700

    testing docker setup

--- dockersetup.sh
+++ dockersetup.sh
@@ -1,4 +1,6 @@
 docker build -t mdwood/fermist .
 docker info
 docker images
-docker run -it mdwood/fermist /bin/bash
+#docker run -it mdwood/fermist /bin/bash
+docker run -d -p 127.0.0.1:80:80 mdwood/fermist
+ls -l
