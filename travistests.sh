a3a1020cc187e4f8fe98fbe846bc526c643aff90
Matthew Wood <mdwood137@gmail.com>
2016-08-11 16:59:39 -0700

    testing docker config

---
+++ travistests.sh
@@ -0,0 +1,20 @@
+
+source condasetup.sh
+
+# This is needed to make matplotlib plot testing work
+#if [[ $TRAVIS_OS_NAME == 'linux' ]] || [[ -z "$DISPLAY" ]]; then
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    export DISPLAY=:99.0;
+    sh -e /etc/init.d/xvfb start;
+    export QT_API=pyqt;
+else
+    export DISPLAY=:99.0;
+    /usr/bin/Xvfb :99 -screen 0 1280x1024x24 &
+fi
+
+python -m pytest -vv -s --cov=fermipy --cov-config=fermipy/tests/coveragerc
+
+if [[ $NAME == 'docs' ]]; then
+    cd docs;
+    sphinx-build -b html -d _build/doctrees . _build/html -W;
+fi
