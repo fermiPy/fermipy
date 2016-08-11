f60155535cdd93d0b87b957855943216c10c149b
Matthew Wood <mdwood137@gmail.com>
2016-08-11 16:53:13 -0700

    testing docker config

--- condasetup.sh
+++ condasetup.sh
@@ -1,8 +1,21 @@
+# Setup script for conda-based ST installations
+
 unset LD_LIBRARY_PATH
+unset DYLD_LIBRARY_PATH
 unset PYTHONPATH
 
-source $FERMI_DIR/fermi-init.sh
 export PATH=$HOME/miniconda/bin:$PATH
 source activate fermi-env
 export PATH=$CONDA_ENV_PATH/bin:$PATH
-export PYTHONPATH=$FERMI_DIR/lib/python:$FERMI_DIR/lib
+
+if [[ $SLAC_ST_BUILD == true ]]; then
+    source $FERMI_DIR/bin/redhat6-x86_64-64bit-gcc44-Optimized/_setup.sh
+    export PYTHONPATH=$FERMI_DIR/python:$FERMI_DIR/lib/redhat6-x86_64-64bit-gcc44-Optimized
+else
+    source $FERMI_DIR/fermi-init.sh
+    export PYTHONPATH=$FERMI_DIR/lib/python:$FERMI_DIR/lib
+fi
+    
+
+
+
