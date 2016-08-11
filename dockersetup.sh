1be289636b0fabd4fb4b16be41e8260b555ef98b
Matthew Wood <mdwood137@gmail.com>
2016-08-11 14:48:44 -0700

    testing docker config

--- dockersetup.sh
+++ dockersetup.sh
@@ -3,5 +3,13 @@
 docker build -t mdwood/fermist .
 docker info
 docker images
-docker run -it -d -v $PWD:/home/fermipy --name=test0 mdwood/fermist /bin/bash
-docker exec test0 /bin/bash -c "cd /home/fermipy;/bin/bash /home/travisinstall.sh"
+docker run -it -d -v $PWD:/home/fermipy --name=test0 \
+       -e GLAST_EXT=/home/externals \
+       -e INST_DIR=/home \
+       -e CONDA_DOWNLOAD="$CONDA_DOWNLOAD" \
+       -e CONDA_DEPS="$CONDA_DEPS" \
+       -e PYTHON_VERSION="$PYTHON_VERSION" \
+       -e CONDA2="$CONDA2" \
+       mdwood/fermist /bin/bash
+docker attach test0
+#docker exec test0 /bin/bash -c "cd /home/fermipy;/bin/bash /home/travisinstall.sh"
