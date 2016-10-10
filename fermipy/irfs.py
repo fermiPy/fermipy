4495d27dfb2fb4baba5bf86ed27ff568435c9cac
Matthew Wood <mdwood137@gmail.com>
2016-10-10 16:53:51 -0700

    Cleanup in irfs module.

--- fermipy/irfs.py
+++ fermipy/irfs.py
@@ -38,12 +38,12 @@ def bitmask_to_bits(mask):
     return bits
 
 
-def poisson_log_like(c,m):
-    return c*np.log(m)-m
+def poisson_log_like(c, m):
+    return c * np.log(m) - m
 
 
-def poisson_ts(sig,bkg):
-    return 2*(poisson_log_like(sig+bkg,sig+bkg) - poisson_log_like(sig+bkg,bkg))
+def poisson_ts(sig, bkg):
+    return 2 * (poisson_log_like(sig + bkg, sig + bkg) - poisson_log_like(sig + bkg, bkg))
 
 
 def compute_ext_flux(egy, flux):
@@ -58,7 +58,7 @@ def compute_ps_loc(egy, flux):
 def compute_ps_counts(ebins, exp, psf, bkg, fn, egy_dim=0):
     """Calculate the observed signal and background counts given models
     for the exposure, background intensity, PSF, and source flux.
-    
+
     Parameters
     ----------
     ebins : `~numpy.ndarray`
@@ -81,18 +81,21 @@ def compute_ps_counts(ebins, exp, psf, bkg, fn, egy_dim=0):
     """
     ewidth = utils.edge_to_width(ebins)
     ectr = np.exp(utils.edge_to_center(np.log(ebins)))
-    
-    theta_edges = np.linspace(0.0,3.0,31)[np.newaxis,:]*np.ones((len(ectr),31))
-    theta_edges *= psf.containment_angle(ectr,fraction=0.68)[:,np.newaxis]
-    theta = 0.5*(theta_edges[:,:-1] + theta_edges[:,1:])
-    domega = np.pi*(theta_edges[:,1:]**2 - theta_edges[:,:-1]**2)
 
-    sig_pdf = domega*psf.interp(ectr[:,np.newaxis], theta)*(np.pi/180.)**2
-    sig_flux = fn.flux(ebins[:-1],ebins[1:])
-    
+    theta_edges = np.linspace(0.0, 3.0, 31)[
+        np.newaxis, :] * np.ones((len(ectr), 31))
+    theta_edges *= psf.containment_angle(ectr, fraction=0.68)[:, np.newaxis]
+    theta = 0.5 * (theta_edges[:, :-1] + theta_edges[:, 1:])
+    domega = np.pi * (theta_edges[:, 1:]**2 - theta_edges[:, :-1]**2)
+
+    sig_pdf = domega * \
+        psf.interp(ectr[:, np.newaxis], theta) * (np.pi / 180.)**2
+    sig_flux = fn.flux(ebins[:-1], ebins[1:])
+
     # Background and signal counts
-    bkgc = bkg[:,np.newaxis]*domega*exp[:,np.newaxis]*ewidth[:,np.newaxis]*(np.pi/180.)**2
-    sigc = sig_pdf*sig_flux[:,np.newaxis]*exp[:,np.newaxis]
+    bkgc = bkg[:, np.newaxis] * domega * exp[:, np.newaxis] * \
+        ewidth[:, np.newaxis] * (np.pi / 180.)**2
+    sigc = sig_pdf * sig_flux[:, np.newaxis] * exp[:, np.newaxis]
 
     return sigc, bkgc
 
@@ -104,7 +107,7 @@ def compute_norm(sig, bkg, ts_thresh, min_counts, sum_axes=None):
     This function uses the Asimov method to calculate the median
     expected TS when the model for the background is fixed (no
     uncertainty on the background amplitude).
-    
+
     Parameters
     ----------
     sig : `~numpy.ndarray`
@@ -126,39 +129,41 @@ def compute_norm(sig, bkg, ts_thresh, min_counts, sum_axes=None):
 
     """
 
-    sig_scale = 10**np.linspace(0.0,5.0,101)
+    sig_scale = 10**np.linspace(0.0, 5.0, 101)
     if sum_axes is None:
         sum_axes = np.arange(sig.ndim)
-    
-    sig = np.expand_dims(sig,-1)
-    bkg = np.expand_dims(bkg,-1)
-    sig_sum = np.apply_over_axes(np.sum,sig,sum_axes)
-    sig_scale = sig_scale*min_counts/sig_sum    
-    ts = np.apply_over_axes(np.sum,poisson_ts(sig*sig_scale,bkg),sum_axes)
+
+    sig = np.expand_dims(sig, -1)
+    bkg = np.expand_dims(bkg, -1)
+    sig_sum = np.apply_over_axes(np.sum, sig, sum_axes)
+    sig_scale = sig_scale * min_counts / sig_sum
+    ts = np.apply_over_axes(np.sum, poisson_ts(sig * sig_scale, bkg), sum_axes)
     vals = np.ones(ts.shape[:-1])
-    
-    for idx, v in np.ndenumerate(ts[...,0]):
-        vals[idx] = np.interp(ts_thresh,ts[idx],sig_scale[idx])
+
+    for idx, v in np.ndenumerate(ts[..., 0]):
+        vals[idx] = np.interp(ts_thresh, ts[idx], sig_scale[idx])
     return vals
-    
+
 
 class Exposure(HpxMap):
 
     def __init__(self, data, hpx):
-        HpxMap.__init__(self,data, hpx)
+        HpxMap.__init__(self, data, hpx)
 
     @staticmethod
     def create(ltc, event_class, event_types, log_energies):
 
-        exp = np.zeros((len(log_energies),ltc.hpx.npix))
+        exp = np.zeros((len(log_energies), ltc.hpx.npix))
         for et in event_types:
             aeff = create_aeff(event_class, et, log_energies, ltc.costh_center)
-            exp += np.sum(aeff.T[:,:,np.newaxis]*ltc.data[:,np.newaxis,:],axis=0)
+            exp += np.sum(aeff.T[:, :, np.newaxis] *
+                          ltc.data[:, np.newaxis, :], axis=0)
 
-        hpx = HPX(ltc.hpx.nside,ltc.hpx.nest,ltc.hpx.coordsys,ebins=log_energies)
+        hpx = HPX(ltc.hpx.nside, ltc.hpx.nest,
+                  ltc.hpx.coordsys, ebins=log_energies)
         return Exposure(exp, hpx)
-            
-        
+
+
 class PSFModel(object):
 
     def __init__(self, skydir, ltc, event_class, event_types,
@@ -182,7 +187,7 @@ class PSFModel(object):
                                                bounds_error=False,
                                                fill_value=None)
 
-        cth_edge = np.linspace(cth_min, 1.0, ncth+1)
+        cth_edge = np.linspace(cth_min, 1.0, ncth + 1)
         cth = edge_to_center(cth_edge)
         ltw = ltc.get_skydir_lthist(skydir, cth_edge)
         for et in event_types:
@@ -199,7 +204,7 @@ class PSFModel(object):
 
         dtheta : array_like
             Array of angular separations in degrees.
-            
+
         scale_fn : callable        
             Function that evaluates the PSF scaling function.
             Argument is energy in MeV.
@@ -225,7 +230,7 @@ class PSFModel(object):
         ----------
         energies : array_like
             Array of energies in MeV.
-        
+
         dtheta : array_like
             Array of angular separations in degrees.
 
@@ -239,9 +244,9 @@ class PSFModel(object):
 
         log_energies = np.log10(energies)
 
-        shape = (energies*dtheta).shape
+        shape = (energies * dtheta).shape
         scale_factor = np.ones(shape)
-        
+
         if scale_fn is not None:
             dtheta = dtheta / scale_fn(energies)
             scale_factor = 1. / scale_fn(energies)**2
@@ -258,7 +263,7 @@ class PSFModel(object):
         if energies is None:
             energies = self.energies
 
-        vals = self.interp(energies[np.newaxis,:], self.dtheta[:,np.newaxis],
+        vals = self.interp(energies[np.newaxis, :], self.dtheta[:, np.newaxis],
                            scale_fn=scale_fn)
         dtheta = np.radians(self.dtheta[:, np.newaxis] * np.ones(vals.shape))
 
@@ -312,7 +317,7 @@ class PSFModel(object):
         if isinstance(event_types, int):
             event_types = bitmask_to_bits(event_types)
 
-        cth_edge = np.linspace(cth_min, 1.0, ncth+1)
+        cth_edge = np.linspace(cth_min, 1.0, ncth + 1)
         cth = edge_to_center(cth_edge)
 
         wpsf = np.zeros((len(dtheta), len(egy)))
@@ -341,8 +346,8 @@ def create_irf(event_class, event_type):
     irfname = '%s::%s' % (event_class, event_type)
     irf = irf_factory.create(irfname)
     return irf
-    
-    
+
+
 def create_psf(event_class, event_type, dtheta, egy, cth):
     """This function creates a map of the PSF versus offset angle.  
 
@@ -392,47 +397,62 @@ def create_aeff(event_class, event_type, egy, cth):
 
 
 class LTCube(HpxMap):
+    """Class for reading and manipulating livetime cubes generated with
+    gtltcube.
+    """
 
     def __init__(self, data, hpx, cth_edges, tstart=None, tstop=None):
-        HpxMap.__init__(self,data, hpx)
+        HpxMap.__init__(self, data, hpx)
         self._cth_edges = cth_edges
         self._cth_center = edge_to_center(self._cth_edges)
         self._cth_width = edge_to_width(self._cth_edges)
-        self._domega = (self._cth_edges[1:]-
-                        self._cth_edges[:-1])*2*np.pi
+        self._domega = (self._cth_edges[1:] -
+                        self._cth_edges[:-1]) * 2 * np.pi
         self._tstart = tstart
         self._tstop = tstop
 
     @property
     def tstart(self):
+        """Return start time."""
         return self._tstart
 
     @property
     def tstop(self):
+        """Return stop time."""
         return self._tstop
-            
+
+    @property
+    def domega(self):
+        """Return solid angle of incidence angle bins in steradians."""
+        return self._domega
+
     @property
     def costh_edges(self):
+        """Return edges of incidence angle bins in cosine of the incidence
+        angle."""
         return self._cth_edges
 
     @property
     def costh_center(self):
+        """Return centers of incidence angle bins in cosine of the incidence
+        angle.
+        """
         return self._cth_center
-    
+
     @staticmethod
     def create(ltfile):
         """Create a livetime cube from a single file or list of
         files."""
-        
+
         if not re.search('\.txt?', ltfile) is None:
-            files = np.loadtxt(ltfile, unpack=True, dtype='str')        
+            files = np.loadtxt(ltfile, unpack=True, dtype='str')
         elif not isinstance(ltfile, list):
             files = glob.glob(ltfile)
 
         ltc = LTCube.create_from_file(files[0])
         for f in files[1:]:
             ltc.load_ltfile(f)
-            
+
         return ltc
 
     @staticmethod
@@ -444,18 +464,18 @@ class LTCube(HpxMap):
         tstop = hdulist[0].header['TSTOP']
         cth_edges = np.array(hdulist['CTHETABOUNDS'].data.field(0))
         cth_edges = np.concatenate(([1], cth_edges))
-        cth_edges = cth_edges[::-1]        
-        hpx = HPX.create_from_header(hdulist['EXPOSURE'].header,cth_edges)
-        return LTCube(data[:,::-1].T,hpx,cth_edges,tstart,tstop)
+        cth_edges = cth_edges[::-1]
+        hpx = HPX.create_from_header(hdulist['EXPOSURE'].header, cth_edges)
+        return LTCube(data[:, ::-1].T, hpx, cth_edges, tstart, tstop)
 
     @staticmethod
-    def create_empty(tstart, tstop, fill=0.0):
+    def create_empty(tstart, tstop, fill=0.0, nside=64):
         """Create an empty livetime cube."""
-        data = np.ones((40,49152))*fill
-        cth_edges = np.linspace(0,1.0,41)
-        hpx = HPX(64,True,'CEL',ebins=cth_edges)    
-        return LTCube(data,hpx,cth_edges,tstart,tstop)        
-    
+        cth_edges = np.linspace(0, 1.0, 41)
+        hpx = HPX(nside, True, 'CEL', ebins=cth_edges)
+        data = np.ones((len(cth_edges) - 1, hpx.npix)) * fill
+        return LTCube(data, hpx, cth_edges, tstart, tstop)
+
     def load_ltfile(self, ltfile):
 
         ltc = LTCube.create_from_file(ltfile)
@@ -481,13 +501,13 @@ class LTCube(HpxMap):
                             (len(cth_edges) - 1) * 4 + 1)
         center = edge_to_center(edges)
         width = edge_to_width(edges)
-
         ipix = hp.ang2pix(self.hpx.nside, np.pi / 2. - np.radians(dec),
                           np.radians(ra), nest=self.hpx.nest)
         lt = np.interp(center, self._cth_center,
-                       self.data[:,ipix] / self._cth_width) * width
+                       self.data[:, ipix] / self._cth_width) * width
         lt = np.sum(lt.reshape(-1, 4), axis=1)
         return lt
+
 
 def plot_hpxmap(hpxmap, **kwargs):
 
@@ -495,29 +515,29 @@ def plot_hpxmap(hpxmap, **kwargs):
     import matplotlib.pyplot as plt
     from matplotlib.colors import PowerNorm, Normalize, LogNorm
 
-    kwargs_imshow = { 'norm' : None,
-                      'vmin' : None, 'vmax' : None }
+    kwargs_imshow = {'norm': None,
+                     'vmin': None, 'vmax': None}
 
-    gamma = kwargs.get('gamma',2.0)
-    zscale = kwargs.get('zscale',None)
-    cbar = kwargs.get('cbar',None)
-    cbar_label = kwargs.get('cbar_label','')
-    title = kwargs.get('title','')
-    levels = kwargs.get('levels',None)
-    rot = kwargs.get('rot',None)
+    gamma = kwargs.get('gamma', 2.0)
+    zscale = kwargs.get('zscale', None)
+    cbar = kwargs.get('cbar', None)
+    cbar_label = kwargs.get('cbar_label', '')
+    title = kwargs.get('title', '')
+    levels = kwargs.get('levels', None)
+    rot = kwargs.get('rot', None)
 
-    kwargs_imshow['vmin'] = kwargs.get('vmin',None)
-    kwargs_imshow['vmax'] = kwargs.get('vmax',None)
+    kwargs_imshow['vmin'] = kwargs.get('vmin', None)
+    kwargs_imshow['vmax'] = kwargs.get('vmax', None)
 
-    cmap = mpl.cm.get_cmap(kwargs.get('cmap','jet'))
+    cmap = mpl.cm.get_cmap(kwargs.get('cmap', 'jet'))
     cmap.set_under('white')
     kwargs_imshow['cmap'] = cmap
 
     if zscale == 'pow':
         vmed = np.median(hpxmap.counts)
         vmax = max(hpxmap.counts)
-        vmin = min(1.1*hpxmap.counts[hpxmap.counts>0])
-        kwargs_imshow['norm'] = PowerNorm(gamma=gamma,clip=True)
+        vmin = min(1.1 * hpxmap.counts[hpxmap.counts > 0])
+        kwargs_imshow['norm'] = PowerNorm(gamma=gamma, clip=True)
     elif zscale == 'log':
         kwargs_imshow['norm'] = LogNorm()
     else:
@@ -528,50 +548,48 @@ def plot_hpxmap(hpxmap, **kwargs):
     fig = plt.gcf()
     if 'sub' in kwargs:
         sub = kwargs['sub']
-        nrows, ncols, idx = sub/100, (sub%100)/10, (sub%10)            
-        c,r = (idx-1)%ncols,(idx-1)/ncols
-        margins = (0.01,0.0,0.0,0.02)
-        extent = (c*1./ncols+margins[0], 
-                  1.-(r+1)*1./nrows+margins[1],
-                  1./ncols-margins[2]-margins[0],
-                  1./nrows-margins[3]-margins[1])
-        extent = (extent[0]+margins[0],
-                  extent[1]+margins[1],
-                  extent[2]-margins[2]-margins[0],
-                  extent[3]-margins[3]-margins[1])
+        nrows, ncols, idx = sub / 100, (sub % 100) / 10, (sub % 10)
+        c, r = (idx - 1) % ncols, (idx - 1) / ncols
+        margins = (0.01, 0.0, 0.0, 0.02)
+        extent = (c * 1. / ncols + margins[0],
+                  1. - (r + 1) * 1. / nrows + margins[1],
+                  1. / ncols - margins[2] - margins[0],
+                  1. / nrows - margins[3] - margins[1])
+        extent = (extent[0] + margins[0],
+                  extent[1] + margins[1],
+                  extent[2] - margins[2] - margins[0],
+                  extent[3] - margins[3] - margins[1])
     else:
-        extent = (0.02,0.05,0.96,0.9)
+        extent = (0.02, 0.05, 0.96, 0.9)
 
-    ax=hp.projaxes.HpxMollweideAxes(fig,extent,coord=None,rot=rot,
-                                    format='%g',flipconv='astro')
+    ax = hp.projaxes.HpxMollweideAxes(fig, extent, coord=None, rot=rot,
+                                      format='%g', flipconv='astro')
 
     ax.set_title(title)
     fig.add_axes(ax)
 
     if hpxmap.data.ndim == 2:
-        data = np.sum(hpxmap.data,axis=0)
+        data = np.sum(hpxmap.data, axis=0)
     else:
         data = hpxmap.data
 
-    img0 = ax.projmap(data,nest=hpxmap.hpx.nest,xsize=1600,coord='C',
+    img0 = ax.projmap(data, nest=hpxmap.hpx.nest, xsize=1600, coord='C',
                       **kwargs_imshow)
 
     if levels:
-        cs = ax.contour(img0,extent=ax.proj.get_extent(),
-                        levels=levels,colors=['k'],
+        cs = ax.contour(img0, extent=ax.proj.get_extent(),
+                        levels=levels, colors=['k'],
                         interpolation='nearest')
 
-    hp.visufunc.graticule(verbose=False,lw=0.5,color='k')
+    hp.visufunc.graticule(verbose=False, lw=0.5, color='k')
 
     if cbar is not None:
 
         im = ax.get_images()[0]
-        cb_kw = dict(orientation = 'vertical', 
+        cb_kw = dict(orientation='vertical',
                      shrink=.6, pad=0.05)
         cb_kw.update(cbar)
-        cb = fig.colorbar(im, **cb_kw)#,format='%.3g')
+        cb = fig.colorbar(im, **cb_kw)  # ,format='%.3g')
         cb.set_label(cbar_label)
-            #, ticks=[min, max])
+        #, ticks=[min, max])
 #            cb.ax.xaxis.set_label_text(cbar_label)
-
-
