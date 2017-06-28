c3d28e945a2736c882fb730263f79b6f928e305f
Eric Charles <badass@stanford.edu>
2017-06-27 18:31:35 -0700

    Fix test_name_factory test

--- fermipy/diffuse/tests/test_name_factory.py
+++ fermipy/diffuse/tests/test_name_factory.py
@@ -22,14 +22,14 @@ def test_name_factory():
     test_dict = name_fact.make_filenames(**names)
     test_irfs = name_fact.irfs(**names)
 
-    assert(test_dict['ltcube'] == 'lt_cubes/ltcube_8years_zmax105.fits')
+    assert(test_dict['ltcube'] == 'lt_cubes/ltcube_8years_none_zmax105.fits')
     assert(test_dict['ft1file'] == 'P8_P302_8years_source_zmax105.lst')
     assert(test_dict['bexpcube'] ==
-           'bexp_cubes/bexcube_P8_P302_8years_source_zmax105_E3_PSF3_GAL_V6.fits')
+           'bexp_cubes/bexcube_P8_P302_8years_source_none_zmax105_E3_PSF3_GAL_V6.fits')
     assert(test_dict[
-           'srcmaps'] == 'srcmaps/srcmaps_ptsrc_P8_P302_8years_source_zmax105_E3_PSF3_GAL_V6.fits')
+           'srcmaps'] == 'srcmaps/srcmaps_ptsrc_P8_P302_8years_source_none_zmax105_E3_PSF3_GAL_V6.fits')
     assert(test_dict[
-           'ccube'] == 'counts_cubes/ccube_P8_P302_8years_source_zmax105_E3_PSF3_GAL.fits')
+           'ccube'] == 'counts_cubes/ccube_P8_P302_8years_source_none_zmax105_E3_PSF3_GAL.fits')
     assert(test_dict[
-           'mcube'] == 'model_cubes/mcube_ptsrc_P8_P302_8years_source_zmax105_E3_PSF3_GAL_V6.fits')
+           'mcube'] == 'model_cubes/mcube_ptsrc_P8_P302_8years_source_none_zmax105_E3_PSF3_GAL_V6.fits')
     assert(test_irfs == 'P8R2_SOURCE_V6')
