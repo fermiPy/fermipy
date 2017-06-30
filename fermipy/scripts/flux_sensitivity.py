b9c96b59b68444a5b3a5e930b1eb230056cf8b3b
Matthew Dunseth Wood <mdwood137@gmail.com>
2017-06-30 16:22:23 -0700

    Move sensitivity classes to separate module.  Add support for vectorizing my sky coordinate and independent data/fit bkg models.

--- fermipy/scripts/flux_sensitivity.py
+++ fermipy/scripts/flux_sensitivity.py
@@ -16,170 +16,9 @@ from fermipy import spectrum
 from fermipy import irfs
 from fermipy import skymap
 from fermipy.ltcube import LTCube
-
-
-class SensitivityCalc(object):
-
-    def __init__(self, gdiff, iso, ltc, ebins, event_class, event_types):
-        """ 
-
-        Parameters
-        ----------
-        gdiff : `~fermipy.skymap.SkyMap`
-            Galactic diffuse map cube object.
-
-        iso : `~numpy.ndarray`
-            Array of background isotropic intensity vs. energy.
-
-        ltc : `~fermipy.ltcube.LTCube`
-
-        event_class : str
-
-        event_types : list        
-
-        """
-        self._gdiff = gdiff
-        self._iso = iso
-        self._ltc = ltc
-        self._ebins = ebins
-        self._log_ebins = np.log10(ebins)
-        self._ectr = np.exp(utils.edge_to_center(np.log(self._ebins)))
-        self._event_class = event_class
-        self._event_types = event_types
-
-        self._psf = []
-        self._exp = []
-
-        ebins = 10**np.linspace(1.0, 6.0, 5 * 8 + 1)
-        skydir = SkyCoord(0.0, 0.0, unit='deg')
-        for et in self._event_types:
-            self._psf += [irfs.PSFModel.create(skydir.icrs, self._ltc,
-                                               self._event_class, et,
-                                               ebins)]
-            self._exp += [irfs.ExposureMap.create(self._ltc,
-                                                  self._event_class, et,
-                                                  ebins)]
-
-    @property
-    def ebins(self):
-        return self._ebins
-
-    @property
-    def ectr(self):
-        return self._ectr
-
-    def compute_counts(self, skydir, fn, ebins=None):
-        """Compute signal and background counts for a point source at
-        position ``skydir`` with spectral parameterization ``fn``.
-
-        Parameters
-        ----------
-        skydir : `~astropy.coordinates.SkyCoord`
-
-        ebins : `~numpy.ndarray`
-
-        Returns
-        -------
-        sig : `~numpy.ndarray`
-            Signal counts array.  Dimensions are energy, angular
-            separation, and event type.
-
-        bkg : `~numpy.ndarray`
-            Background counts array.  Dimensions are energy, angular
-            separation, and event type.
-
-        """
-
-        if ebins is None:
-            ebins = self.ebins
-            ectr = self.ectr
-        else:
-            ectr = np.exp(utils.edge_to_center(np.log(ebins)))
-
-        skydir_cel = skydir.transform_to('icrs')
-        skydir_gal = skydir.transform_to('galactic')
-
-        sig = []
-        bkg = []
-        for psf, exp in zip(self._psf, self._exp):
-            expv = exp.interpolate(
-                skydir_cel.icrs.ra.deg, skydir_cel.icrs.dec.deg, ectr)
-            bkgv = self._gdiff.interpolate(
-                skydir_gal.l.deg, skydir_gal.b.deg, ectr)
-            isov = np.exp(np.interp(np.log(ectr), np.log(self._iso[0]),
-                                    np.log(self._iso[1])))
-            bkgv += isov
-            s, b = irfs.compute_ps_counts(ebins, expv, psf, bkgv, fn)
-            sig += [s]
-            bkg += [b]
-
-        sig = np.concatenate([np.expand_dims(t, -1) for t in sig])
-        bkg = np.concatenate([np.expand_dims(t, -1) for t in bkg])
-
-        return sig, bkg
-
-    def diff_flux_threshold(self, skydir, fn, ts_thresh, min_counts):
-        """Compute the differential flux threshold for a point source at
-        position ``skydir`` with spectral parameterization ``fn``.
-
-        """
-
-        sig, bkg = self.compute_counts(skydir, fn)
-
-        norms = irfs.compute_norm(sig, bkg, ts_thresh,
-                                  min_counts, sum_axes=[1, 2])
-        npred = np.squeeze(np.apply_over_axes(np.sum, norms * sig, [1, 2]))
-        norms = np.squeeze(norms)
-        flux = norms * fn.flux(self.ebins[:-1], self.ebins[1:])
-        eflux = norms * fn.eflux(self.ebins[:-1], self.ebins[1:])
-        dnde = norms * fn.dnde(self.ectr)
-        e2dnde = self.ectr**2 * dnde
-
-        return dict(e_min=self.ebins[:-1], e_max=self.ebins[1:],
-                    e_ref=self.ectr,
-                    npred=npred, flux=flux, eflux=eflux,
-                    dnde=dnde, e2dnde=e2dnde)
-
-    def int_flux_threshold(self, skydir, fn, ts_thresh, min_counts):
-        """Compute the integral flux threshold for a point source at
-        position ``skydir`` with spectral parameterization ``fn``.
-
-        """
-
-        ebins = 10**np.linspace(np.log10(self.ebins[0]),
-                                np.log10(self.ebins[-1]), 32)
-        ectr = np.sqrt(ebins[0] * ebins[-1])
-
-        sig, bkg = self.compute_counts(skydir, fn, ebins)
-
-        norms = irfs.compute_norm(sig, bkg, ts_thresh,
-                                  min_counts, sum_axes=[0, 1, 2])
-        npred = np.squeeze(np.apply_over_axes(np.sum, norms * sig, [0, 1, 2]))
-        norms = np.squeeze(norms)
-        flux = norms * fn.flux(ebins[0], ebins[-1])
-        eflux = norms * fn.eflux(ebins[0], ebins[-1])
-        dnde = norms * fn.dnde(ectr)
-        e2dnde = ectr**2 * dnde
-
-        o = dict(e_min=self.ebins[0], e_max=self.ebins[-1], e_ref=ectr,
-                 npred=npred, flux=flux, eflux=eflux,
-                 dnde=dnde, e2dnde=e2dnde)
-
-        sig, bkg = self.compute_counts(skydir, fn)
-        npred = np.squeeze(np.apply_over_axes(np.sum, norms * sig, [1, 2]))
-        flux = norms * fn.flux(self.ebins[:-1], self.ebins[1:])
-        eflux = norms * fn.eflux(self.ebins[:-1], self.ebins[1:])
-        dnde = norms * fn.dnde(self.ectr)
-        e2dnde = ectr**2 * dnde
-
-        o['bins'] = dict(npred=npred,
-                         flux=flux,
-                         eflux=eflux,
-                         dnde=dnde,
-                         e2dnde=e2dnde,
-                         e_min=self.ebins[:-1], e_max=self.ebins[1:], e_ref=self.ectr)
-
-        return o
+from fermipy.sensitivity import SensitivityCalc
+from fermipy.hpx_utils import HPX
+from fermipy.skymap import HpxMap
 
 
 def main(args=None):
@@ -194,6 +33,19 @@ def main(args=None):
     parser.add_argument('--isodiff', default=None,
                         help='Set the path to the isotropic model.  If none then the '
                         'default model will be used for the given event class.')
+
+    parser.add_argument('--galdiff_fit', default=None,
+                        help='Set the path to the galactic diffuse model used for fitting. '
+                        'This can be used to assess the impact of IEM systematics  '
+                        'from fitting with the wrong model.  If none '
+                        'then the same model will be used for data and fit.')
+    
+    parser.add_argument('--isodiff_fit', default=None,
+                        help='Set the path to the isotropic model used for fitting.  '
+                        'This can be used to assess the impact of IEM systematics  '
+                        'from fitting with the wrong model.  If none '
+                        'then the same model will be used for data and fit.')
+    
     parser.add_argument('--ts_thresh', default=25.0, type=float,
                         help='Set the detection threshold.')
     parser.add_argument('--min_counts', default=3.0, type=float,
@@ -212,6 +64,10 @@ def main(args=None):
                         help='Maximum energy in MeV.')
     parser.add_argument('--nbin', default=18, type=int,
                         help='Number of energy bins for differential flux calculation.')
+    parser.add_argument('--nside', default=None, type=int,
+                        help='Set the NSIDE parameter of the all-sky sensivity map.  If None then no sensitivity maps '
+                        'will be computed.  WARNING: Running with a large nside parameter is very '
+                        'computationally and memory intensive.')
     parser.add_argument('--output', default='output.fits', type=str,
                         help='Output filename.')
     parser.add_argument('--obs_time_yr', default=None, type=float,
@@ -233,10 +89,14 @@ def run_flux_sensitivity(**kwargs):
     ltcube_filepath = kwargs.get('ltcube', None)
     galdiff_filepath = kwargs.get('galdiff', None)
     isodiff_filepath = kwargs.get('isodiff', None)
+    galdiff_fit_filepath = kwargs.get('galdiff_fit', None)
+    isodiff_fit_filepath = kwargs.get('isodiff_fit', None)
+    
     obs_time_yr = kwargs.get('obs_time_yr', None)
     event_class = kwargs.get('event_class', 'P8R2_SOURCE_V6')
     min_counts = kwargs.get('min_counts', 3.0)
     ts_thresh = kwargs.get('ts_thresh', 25.0)
+    nside = kwargs.get('nside', None)
     output = kwargs.get('output', None)
 
     event_types = [['FRONT', 'BACK']]
@@ -262,7 +122,10 @@ def run_flux_sensitivity(**kwargs):
                 24 * 3600. / (ltc.tstop - ltc.tstart)
 
     gdiff = skymap.Map.create_from_fits(galdiff_filepath)
-
+    gdiff_fit = None
+    if galdiff_fit_filepath is not None:
+        gdiff_fit = skymap.Map.create_from_fits(galdiff_fit_filepath)
+        
     if isodiff_filepath is None:
         isodiff = utils.resolve_file_path('iso_%s_v06.txt' % event_class,
                                           search_dirs=[os.path.join('$FERMIPY_ROOT', 'data'),
@@ -271,13 +134,44 @@ def run_flux_sensitivity(**kwargs):
     else:
         isodiff = isodiff_filepath
 
+
     iso = np.loadtxt(isodiff, unpack=True)
-
+    iso_fit = None
+    if isodiff_fit_filepath is not None:
+        iso_fit = np.loadtxt(isodiff_fit_filepath, unpack=True)
+    
     scalc = SensitivityCalc(gdiff, iso, ltc, ebins,
-                            event_class, event_types)
+                            event_class, event_types, gdiff_fit=gdiff_fit,
+                            iso_fit=iso_fit)
 
+    # Compute Maps
+    map_diff_flux = None
+    map_diff_npred = None
+    map_int_flux = None
+    map_int_npred = None
+    
+    if nside is not None:
+
+        hpx = HPX(nside, True, 'GAL', ebins=ebins)
+        map_diff_flux = HpxMap(np.zeros((nbin,hpx.npix)),hpx)
+        map_diff_npred = HpxMap(np.zeros((nbin,hpx.npix)),hpx)
+        map_skydir = map_diff_flux.hpx.get_sky_dirs()
+
+        o = scalc.diff_flux_threshold(map_skydir, fn, ts_thresh, min_counts)
+        map_diff_flux.data[...] = o['flux'].T
+        map_diff_npred.data[...] = o['npred'].T
+
+        hpx = HPX(nside, True, 'GAL')
+        map_int_flux = HpxMap(np.zeros((hpx.npix)),hpx)
+        map_int_npred = HpxMap(np.zeros((hpx.npix)),hpx)
+        map_skydir = map_int_flux.hpx.get_sky_dirs()
+
+        o = scalc.int_flux_threshold(map_skydir, fn, ts_thresh, min_counts)
+        map_int_flux.data[...] = o['flux']
+        map_int_npred.data[...] = o['npred']
+        
     o = scalc.diff_flux_threshold(c, fn, ts_thresh, min_counts)
-
+        
     cols = [Column(name='e_min', dtype='f8', data=scalc.ebins[:-1], unit='MeV'),
             Column(name='e_ref', dtype='f8', data=o['e_ref'], unit='MeV'),
             Column(name='e_max', dtype='f8', data=scalc.ebins[1:], unit='MeV'),
@@ -352,6 +246,21 @@ def run_flux_sensitivity(**kwargs):
     hdulist[2].name = 'INT_FLUX'
     hdulist[3].name = 'INT_FLUX_EBIN'
 
+    if nside is not None:
+        hdu = map_diff_flux.create_image_hdu()
+        hdu.name = 'MAP_DIFF_FLUX'
+        hdulist.append(hdu)
+        hdu = map_diff_npred.create_image_hdu()
+        hdu.name = 'MAP_DIFF_NPRED'
+        hdulist.append(hdu)
+
+        hdu = map_int_flux.create_image_hdu()
+        hdu.name = 'MAP_INT_FLUX'
+        hdulist.append(hdu)
+        hdu = map_int_npred.create_image_hdu()
+        hdu.name = 'MAP_INT_NPRED'
+        hdulist.append(hdu)
+        
     hdulist.writeto(output, clobber=True)
 
 if __name__ == "__main__":
