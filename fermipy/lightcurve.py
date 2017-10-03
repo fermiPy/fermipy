e717356599dc7c628123283910457aa34ec9988c
Matthew Dunseth Wood <mdwood137@gmail.com>
2017-10-02 19:01:03 -0700

    Refactoring to allow ROI state object to be used to initialize analysis for each lightcurve bin.

--- fermipy/lightcurve.py
+++ fermipy/lightcurve.py
@@ -31,6 +31,7 @@ from astropy.table import Table, Column
 
 import pyLikelihood as pyLike
 
+
 def _fit_lc(gta, name, **kwargs):
 
     # lightcurve fitting routine-
@@ -63,26 +64,26 @@ def _fit_lc(gta, name, **kwargs):
             gta.free_sources_by_name(free_sources)
         elif niter == 1:
             gta.logger.info('Fit Failed. Retrying with free '
-                             'normalizations.')
+                            'normalizations.')
             gta.free_sources_by_name(free_sources, False)
             gta.free_sources_by_name(free_sources, pars='norm')
         elif niter == 2:
             gta.logger.info('Fit Failed with User Supplied List of '
-                             'Free/Fixed Sources.....Lets try '
-                             'fixing TS<4 sources')
+                            'Free/Fixed Sources.....Lets try '
+                            'fixing TS<4 sources')
             gta.free_sources_by_name(free_sources, False)
             gta.free_sources_by_name(free_sources, pars='norm')
             gta.free_sources(minmax_ts=[0, 4], free=False, exclude=[name])
         elif niter == 3:
             gta.logger.info('Fit Failed with User Supplied List of '
-                             'Free/Fixed Sources.....Lets try '
-                             'fixing TS<9 sources')
+                            'Free/Fixed Sources.....Lets try '
+                            'fixing TS<9 sources')
             gta.free_sources_by_name(free_sources, False)
             gta.free_sources_by_name(free_sources, pars='norm')
             gta.free_sources(minmax_ts=[0, 9], free=False, exclude=[name])
         elif niter == 4:
             gta.logger.info('Fit still did not converge, lets try fixing the '
-                             'sources up to 1dg out from ROI')
+                            'sources up to 1dg out from ROI')
             gta.free_sources_by_name(free_sources, False)
             for s in free_sources:
                 src = gta.roi.get_source_by_name(s)
@@ -91,7 +92,7 @@ def _fit_lc(gta, name, **kwargs):
             gta.free_sources(minmax_ts=[0, 9], free=False, exclude=[name])
         else:
             gta.logger.error('Fit still didnt converge.....please examine this data '
-                              'point, setting output to 0')
+                             'point, setting output to 0')
             break
 
         fit_results = gta.fit()
@@ -100,17 +101,16 @@ def _fit_lc(gta, name, **kwargs):
 
     return fit_results
 
-def _process_lc_bin(itime, name, config, basedir, workdir, diff_sources, const_spectrum, **kwargs):
+
+def _process_lc_bin(itime, name, config, basedir, workdir, diff_sources, const_spectrum, roi,
+                    **kwargs):
     i, time = itime
+    
+    roi = copy.deepcopy(roi)
 
     config = copy.deepcopy(config)
     config['selection']['tmin'] = time[0]
     config['selection']['tmax'] = time[1]
-    config['ltcube']['use_local_ltcube'] = kwargs['use_local_ltcube']
-    config['model']['diffuse_dir'] = [workdir]
-
-    if config['components'] is None:
-        config['components'] = []
 
     # create output directories labeled in MET vals
     outdir = basedir + 'lightcurve_%.0f_%.0f' % (time[0], time[1])
@@ -131,26 +131,13 @@ def _process_lc_bin(itime, name, config, basedir, workdir, diff_sources, const_s
 
     try:
         from fermipy.gtanalysis import GTAnalysis
-        gta = GTAnalysis(config)
+        gta = GTAnalysis(config, roi, loglevel=logging.DEBUG)
         gta.logger.info('Fitting time range %i %i' % (time[0], time[1]))
-        for j, c in enumerate(gta.components):
-            if len(config['components']) <= j:
-                config['components'] += [{}]
-
-            data_cfg = {'evfile': c.files['ft1'],
-                        'scfile': c.data_files['scfile'],
-                        'ltcube': None}
-
-            config['components'][j] = \
-                utils.merge_dict(config['components'][j],
-                                 {'data': data_cfg},
-                                 add_new_keys=True)
-
         gta.setup()
-        gta.load_roi(workdir+'/_lc_%s.npy'%name)
+        # gta.load_roi(workdir+'/_lc_%s.npy'%name)
     except:
-        print('Analysis failed in time range %i %i'%
-                            (time[0], time[1]))
+        print('Analysis failed in time range %i %i' %
+              (time[0], time[1]))
         print(sys.exc_info()[0])
         raise
         return {}
@@ -160,52 +147,50 @@ def _process_lc_bin(itime, name, config, basedir, workdir, diff_sources, const_s
 
     # Optimize the model
     gta.optimize(skip=diff_sources)
-    gta.optimize(skip=diff_sources)
 
     fit_results = _fit_lc(gta, name, **kwargs)
     gta.write_xml('fit_model_final.xml')
     srcmodel = copy.deepcopy(gta.get_src_model(name))
 
-    # rerun fit using params from full time (constant) fit using same 
+    # rerun fit using params from full time (constant) fit using same
     # param vector as the successful fit to get loglike
     specname, spectrum = const_spectrum
-    gta.set_source_spectrum(name, spectrum_type=specname, 
-            spectrum_pars=spectrum)
+    gta.set_source_spectrum(name, spectrum_type=specname,
+                            spectrum_pars=spectrum)
     gta.free_source(name, free=False)
     const_fit_results = gta.fit()
     const_srcmodel = gta.get_src_model(name)
 
     o = {'flux_const': const_srcmodel['flux'],
-            'loglike_flux_const': const_fit_results['loglike'],
-            'fit_success': fit_results['fit_success']}
+         'loglike_const': const_fit_results['loglike'],
+         'fit_success': fit_results['fit_success']}
 
     if fit_results['fit_success'] == 1:
         for k in defaults.source_flux_output.keys():
             if not k in srcmodel:
                 continue
-            """if (isinstance(srcmodel[k], np.ndarray) and
-                    o[k][i].shape != output[k].shape):
-                gta.logger.warning(
-                    'Incompatible shape for column %s', k)
-                continue"""
             o[k] = srcmodel[k]
-    
+
     gta.logger.info('Finished time range %i %i' % (time[0], time[1]))
 
     return o
 
-def calcTS_var(loglike, loglike_flux_const, flux_err, flux_const, systematic):
-    # calculates variability according to Eq. 4 in 2FGL 
+
+def calcTS_var(loglike, loglike_const, flux_err, flux_const, systematic):
+    # calculates variability according to Eq. 4 in 2FGL
     # including correction using non-numbered Eq. following Eq. 4
 
     # first, remove failed bins
     loglike = [elm for elm in loglike if isinstance(elm, float)]
-    loglike_flux_const = [elm for elm in loglike_flux_const if isinstance(elm, float)]
+    loglike_const = [
+        elm for elm in loglike_const if isinstance(elm, float)]
     flux_err = [elm for elm in flux_err if isinstance(elm, float)]
 
-    v_sqs = [loglike[i] - loglike_flux_const[i] for i in xrange(len(loglike))]
-    factors = [flux_err[i]**2 / (flux_err[i]**2 + systematic**2 * flux_const**2) for i in xrange(len(flux_err))]
-    return 2.*np.sum([a*b for a,b in zip(factors, v_sqs)])
+    v_sqs = [loglike[i] - loglike_const[i] for i in xrange(len(loglike))]
+    factors = [flux_err[i]**2 / (flux_err[i]**2 + systematic**2 * flux_const**2)
+               for i in xrange(len(flux_err))]
+    return 2. * np.sum([a * b for a, b in zip(factors, v_sqs)])
+
 
 class LightCurve(object):
 
@@ -298,6 +283,7 @@ class LightCurve(object):
         o['tmax'] = times[1:]
         o['tmin_mjd'] = utils.met_to_mjd(o['tmin'])
         o['tmax_mjd'] = utils.met_to_mjd(o['tmax'])
+        o['loglike_const'] = np.nan * np.ones(o['tmin'].shape)
 
         for k, v in defaults.source_flux_output.items():
 
@@ -327,33 +313,48 @@ class LightCurve(object):
             times = np.arange(self.tmin, self.tmax,
                               kwargs['binsz'])
 
-        o = self._create_lc_dict(name, times)
-        o['config'] = kwargs
-
         diff_sources = [s.name for s in self.roi.sources if s.diffuse]
         skydir = self.roi[name].skydir
 
         if kwargs.get('free_radius', None) is not None:
-            kwargs['free_sources'] += [s.name for s in
-                                       self.roi.get_sources(skydir=skydir,
-                                                            distance=kwargs[
-                                                                'free_radius'],
-                                                            exclude=diff_sources)]
+            kwargs['free_sources'] += [
+                s.name for s in self.roi.get_sources(skydir=skydir,
+                                                     distance=kwargs['free_radius'],
+                                                     exclude=diff_sources)]
 
-        self.write_roi('_lc_%s'%name, make_plots=False, save_model_map=False)
-        self.optimize()
-        self.optimize()
+        #self.write_roi('_lc_%s'%name, make_plots=False, save_model_map=False)
+        # self.optimize()
+        # self.optimize()
         # save params from full time fit
         spectrum = self.like[name].src.spectrum()
         specname = spectrum.genericName()
         const_spectrum = (specname, gtutils.get_function_pars_dict(spectrum))
 
+        # Create Configurations
+        config = copy.deepcopy(self.config)
+        config['ltcube']['use_local_ltcube'] = kwargs['use_local_ltcube']
+        config['model']['diffuse_dir'] = [self.workdir]
+        if config['components'] is None:
+            config['components'] = []
+        for j, c in enumerate(self.components):
+            if len(config['components']) <= j:
+                config['components'] += [{}]
+
+            data_cfg = {'evfile': c.files['ft1'],
+                        'scfile': c.data_files['scfile'],
+                        'ltcube': None}
+
+            config['components'][j] = \
+                utils.merge_dict(config['components'][j],
+                                 {'data': data_cfg},
+                                 add_new_keys=True)
+
         directory = kwargs.get('directory', None)
         basedir = directory + '/' if directory is not None else ''
         mt = kwargs.get('multithread', False)
-        wrap = partial(_process_lc_bin, name=name, config=self.config,
-                basedir=basedir, workdir=self.workdir, diff_sources=diff_sources, 
-                const_spectrum=const_spectrum, **kwargs)
+        wrap = partial(_process_lc_bin, name=name, config=config,
+                       basedir=basedir, workdir=self.workdir, diff_sources=diff_sources,
+                       const_spectrum=const_spectrum, roi=self.roi, **kwargs)
         itimes = enumerate(zip(times[:-1], times[1:]))
         if mt:
             p = Pool()
@@ -361,31 +362,38 @@ class LightCurve(object):
         else:
             mapo = map(wrap, itimes)
 
+        o = self._create_lc_dict(name, times)
+        o['config'] = kwargs
+
         itimes = enumerate(zip(times[:-1], times[1:]))
         for i, time in itimes:
+
             if not mapo[i]['fit_success']:
-                self.logger.error('Fit failed in bin %d in range %i %i.'%(i,time[0],time[1]))
-                mapo[i] = utils.merge_dict(mapo[i], defaults.source_flux_output, add_new_keys=True)
+                self.logger.error(
+                    'Fit failed in bin %d in range %i %i.' % (i, time[0], time[1]))
+                continue
 
-        for k in mapo[0].keys():
-            if k in o:
-                o.pop(k, None)
+            for k in o.keys():
+                if not k in mapo[i]:
+                    continue
+                # if (isinstance(o[k], np.ndarray) and
+                #    o[k][i].shape != mapo[i][k].shape):
+                #    gta.logger.warning('Incompatible shape for column %s', k)
+                #    continue
 
-        files = os.listdir('./')
-        for file in files:
-            if file.startswith('_lc_%s'%name):
-                os.remove(file)
+                try:
+                    o[k][i] = mapo[i][k]
+                except:
+                    pass
 
-        merged = utils.merge_list_of_dicts(mapo)
-        o = utils.merge_dict(o, merged, add_new_keys=True)
-
+        #merged = utils.merge_list_of_dicts(mapo)
+        #o = utils.merge_dict(o, merged, add_new_keys=True)
         systematic = kwargs.get('systematic', 0.02)
 
-        o['ts_var'] = calcTS_var(loglike=o['loglike'], 
-                loglike_flux_const=o['loglike_flux_const'],
-                flux_err=o['flux_err'],
-                flux_const=o['flux_const'][0],
-                systematic=systematic)
+        o['ts_var'] = calcTS_var(loglike=o['loglike'],
+                                 loglike_const=o['loglike_const'],
+                                 flux_err=o['flux_err'],
+                                 flux_const=mapo[0]['flux_const'],
+                                 systematic=systematic)
 
         return o
-
