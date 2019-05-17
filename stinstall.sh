cb767446bf44d9bcacdc25c050b677efa47ae7de
Eric Charles <echarles@gmail.com>
2019-05-17 14:56:25 -0700

    Fixing up travis build

--- stinstall.sh
+++ stinstall.sh
@@ -2,7 +2,7 @@
 #export ST_URL="https://www.dropbox.com/s/b5z0wfc0xt9n0ha/ScienceTools-v10r0p5-fssc-20150518-x86_64-unknown-linux-gnu-libc2.19-10-without-rootA-light.tar.gz"
 #export ST_URL="https://www.slac.stanford.edu/~mdwood/fermipy/ScienceTools-v10r0p5-fssc-20150518-x86_64-unknown-linux-gnu-libc2.19-10-without-rootA-light.tar.gz"
 
-conda install -c conda-forge/label/cf201901 -c fermi fermitools
+#conda install -c conda-forge/label/cf201901 -c fermi fermitools
 
 #if [[ ! -d "$HOME/ScienceTools" || -n "$1" ]]; then
 #if [ -n "$1" ]; then
