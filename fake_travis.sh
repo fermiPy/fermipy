1b980b8ffb6dff305dbc177b2d1b5adf813c40d7
Eric Charles <echarles@gmail.com>
2019-05-22 13:07:07 -0700

    Still playing with travis

--- fake_travis.sh
+++ fake_travis.sh
@@ -5,13 +5,58 @@ export CONDA2='conda install -y -c conda-forge healpy'
 export INSTALL_CMD='python setup.py install'
 export CONDA_CHANNELS="conda-forge"
 export CONDA_DEPS='gammapy numpy astropy scipy matplotlib pytest pyyaml'
-export PYTHON_VERSION="2.7"
-export CONDA_DOWNLOAD="Miniconda-latest-Linux-x86_64.sh"
 
-export ST_INSTALL="conda install -y -c conda-forge/label/cf201901 -c fermi fermitools"
-export CONDA2="conda install -y -c conda-forge healpy"
-export INSTALL_CMD='python setup.py install'
-export CONDA_PATH='/u/ek/echarles/dmcat/software/build_test/miniconda'
+
+if [ $# -gt 0 ]; then
+    NAME=$1
+else
+    NAME='main'
+fi
+
+
+case $NAME in
+    main)
+	export PYTHON_VERSION="2.7"
+	export CONDA_DOWNLOAD="Miniconda-latest-Linux-x86_64.sh"
+	export ST_INSTALL="conda install -y -c conda-forge/label/cf201901 -c fermi fermitools"
+	export CONDA_PATH='/u/ek/echarles/dmcat/software/build_test/miniconda'
+	;;
+    old)
+	export PYTHON_VERSION="2.7"
+	export CONDA_DOWNLOAD="Miniconda-latest-Linux-x86_64.sh"
+	export ST_INSTALL="conda install -y -c conda-forge/label/cf201901 -c fermi fermitools=1.0.2"
+	export CONDA_PATH='/u/ek/echarles/dmcat/software/build_test/miniconda'
+	;;
+    ancient)
+	export PYTHON_VERSION="2.7"
+	export CONDA_DOWNLOAD="Miniconda-latest-Linux-x86_64.sh"
+	export ST_PATH='/u/ek/echarles/dmcat/software/build_test'
+	export ST_INSTALL="bash stinstall.sh $ST_PATH"
+	export CONDA_PATH='/u/ek/echarles/dmcat/software/build_test/miniconda'
+	;;
+    docs)
+	export PYTHON_VERSION="3.6"
+        export ST_INSTALL=''
+        export DOCKER_INSTALL=''
+        export PIP_DEPS='coverage pytest-cov coveralls sphinx sphinx_rtd_theme'
+	export CONDA_DOWNLOAD='Miniconda3-latest-Linux-x86_64.sh'
+        export CONDA_DEPS='gammapy numpy astropy scipy matplotlib pytest pyyaml sphinx sphinx_rtd_theme'
+	;;
+    py36_st-no_dep-yes)
+	export PYTHON_VERSION="3.6"
+        export ST_INSTALL=''
+        export DOCKER_INSTALL=''
+	export CONDA_DOWNLOAD='Miniconda3-latest-Linux-x86_64.sh'
+	;;
+    py2_st-no_dep-yes)
+	export PYTHON_VERSION="2.7"
+        export ST_INSTALL=''
+        export DOCKER_INSTALL=''
+	export CONDA_DOWNLOAD='Miniconda-latest-Linux-x86_64.sh'
+	;;
+esac
+
+echo "Running fake_travis for build $NAME "
 
 \rm -rf $CONDA_PATH
 source condainstall.sh $CONDA_PATH
@@ -19,4 +64,4 @@ $ST_INSTALL
 
 bash travistests.sh
 
-coveralls --rcfile='fermipy/tests/coveragerc
+coveralls --rcfile='fermipy/tests/coveragerc'
