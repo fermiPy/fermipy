0c6f66eeb6e8dd9cb43417c6249833db5af106fa
mattiadimauro <mdimauro@slac.stanford.edu>
2019-09-18 07:54:20 -0700

    Small change into fermipy/jobs/target_sim.py

--- fermipy/jobs/target_sim.py
+++ fermipy/jobs/target_sim.py
@@ -329,7 +329,6 @@ class SimulateROI(Link):
                 gta.free_source(src_name, pars='norm')
 
 	    gta.sed(test_source_name, outfile=sedfile)
-            #gta.sed(test_source_name, prefix=pkey, outfile=sedfile)
             # Set things back to how they were
             gta.delete_source(test_source_name)
             gta.load_xml('sim_refit_%06i' % current_seed)
