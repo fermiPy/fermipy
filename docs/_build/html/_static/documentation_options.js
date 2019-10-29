c0d3e23917021c557eed0aaf9fd02158d253d5a6
mattiadimauro <mdimauro@slac.stanford.edu>
2019-10-29 14:01:13 -0700

    Changes to documentation

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0+25.g938c.dirty',
+    VERSION: '0.18.0+28.g7c523',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
