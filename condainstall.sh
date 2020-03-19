24be88fa8021282978bd1a59e9df37aaddd89f57
Eric Charles <echarles@gmail.com>
2020-03-19 09:25:02 -0700

    more travis testing

--- condainstall.sh
+++ condainstall.sh
@@ -19,7 +19,7 @@ if [[ -z $CONDA_DEPS ]]; then
 fi
     
 if [[ -z $CONDA2 ]]; then
-    CONDA2='conda install -y healpy subprocess32 gammapy pytest'
+    CONDA2='conda install -y --name fermipy-test-build healpy subprocess32 pytest'
 fi
 
 if [[ -z $CONDA_PATH ]]; then
@@ -40,15 +40,18 @@ else
     echo "Using existing conda installation under $CONDA_PATH"
 fi
 
+# First we update conda and make an env
+conda update -q conda -y
+conda create --name fermipy-test-build -c conda-forge -y python=$PYTHON_VERSION
+conda activate fermipy-test-build
+
+# Install the science tools, if requested
 if [[ -n $ST_INSTALL ]]; then
     $ST_INSTALL
 fi
 
-conda update -q conda -y
-conda create --name fermipy-test-build -c conda-forge -y python=$PYTHON_VERSION
 conda install -n fermipy-test-build -y $CONDA_DEPS
-conda install -n fermipy-test-build --only-deps -y fermipy
-conda activate fermipy-test-build
+#conda install -n fermipy-test-build --only-deps -y fermipy
 
 if [[ -n $CONDA2 ]]; then
     $CONDA2
