54581719c686cd7b2a3bc6f7953037bbdfc0ad58
Eric Charles <echarles@gmail.com>
2020-03-22 15:24:01 -0700

    debbuing for travis

--- fermipy/tests/test_gtanalysis.py
+++ fermipy/tests/test_gtanalysis.py
@@ -202,6 +202,9 @@ def test_gtanalysis_find_sources(create_draco_analysis):
     newsrcs1 = gta.get_sources(skydir=src1.skydir, distance=0.3,
                                exclude=diff_sources)
 
+    print(newsrcs0)
+    print(newsrcs1)
+    
     assert(len(newsrcs0) == 1)
     assert(len(newsrcs1) == 1)
 
