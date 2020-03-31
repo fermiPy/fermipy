0cdb4323dc4a960441a14bf1fc67a1ba4651d05b
Eric Charles <echarles@gmail.com>
2020-03-31 11:34:44 -0700

    recover from single failed in lc

--- fermipy/lightcurve.py
+++ fermipy/lightcurve.py
@@ -160,7 +160,6 @@ def _process_lc_bin(itime, name, config, basedir, workdir, diff_sources, const_s
         print('Analysis failed in time range %i %i' %
               (time[0], time[1]))
         print(sys.exc_info()[0])
-        raise
         return {}
 
     gta._lck_params = lck_params
