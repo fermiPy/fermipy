7a1915538105593e990f7760fddb7d922c577222
ndilalla <niccolo.dilalla@stanford.edu>
2026-03-04 07:30:02 -0800

    Updating tables for docs.

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Scientific/Engineering :: Astronomy',
         'Development Status :: 5 - Production/Stable',
